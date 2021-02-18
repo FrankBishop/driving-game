7f1f9b3ee2271a178f6f409208bbce2d56f76f9f
Frank Bishop <ffbishop3@gmail.com>
2021-02-18 12:38:48 -0800

    Create a simple data model that includes which way the car is facing, (north, south, east, or west).

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // var carIcon =  document.querySelector('.car');
+
+// var carDirection = [
+//   {
+//   direction: 'left'
+//   },
+//   {
+//   direction: 'right'
+//   },
+//   {
+//   direction: 'up'
+//   },
+//   {
+//   direction: 'down'
+//   }
+// ]
